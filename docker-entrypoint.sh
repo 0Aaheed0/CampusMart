267703b77ba05f67382089e4a1be6e9592ec0b66
0Aaheed0 <aaheed.28@gmail.com>
2026-04-11 04:38:10 +0600

    Fix entrypoint loop with exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-php artisan config:clear
-php artisan cache:clear
+php artisan config:clear || true
+php artisan cache:clear || true
 php artisan migrate:fresh --force
 php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 
-apache2ctl -D FOREGROUND
+exec apache2ctl -D FOREGROUND
