8a49370e035931915484b5c35f1db981a86731b9
0Aaheed0 <aaheed.28@gmail.com>
2026-04-11 04:21:05 +0600

    Use migrate:fresh to fix duplicate seeding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 php artisan config:clear
 php artisan cache:clear
-php artisan migrate --force
+php artisan migrate:fresh --force
 php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
